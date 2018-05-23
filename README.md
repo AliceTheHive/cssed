8e823bf51351c3fa63132a37a76340ba148ab0da
Andre Santos <NodePoint@users.noreply.github.com>
2018-05-23 16:28:03 +0100

    Initial upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# CSSED
+
+Cloudflare ScrapeShield Email Deobfuscator
+
+## About
+
+CSSED is a Python 3.x script based off of the final code at https://blog.jli.host/posts/cloudflare-scrape-shield/, ported from JavaScript to Python.
+<br>
+This decodes the hex string appended to `/cdn-cgi/l/email-protection#`. For instance, `/cdn-cgi/l/email-protection#6e040b1d1d0b040b1d1d0b5f5c5d2e09030f0702400d0103`.
+
+## Prerequisites
+
+- Python 3.x
+
+## Usage
+
+./cssed.py
+
+## Tested
+- Windows 10 Pro
---
+++ cssed.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import urllib.parse
+
+def hex_at(string, index, mode):
+    if (mode == 0):
+        r = string[index:2]
+    else:
+        r = string[int(index-2):index]
+    return int(r, 16)
+
+def decrypt(ciphertext):
+    output = ""
+    i = 2
+    key = hex_at(ciphertext, 0, 0)
+    while i < len(ciphertext):
+        i += 2
+        plaintext = hex_at(ciphertext, i, 1) ^ key
+        output += chr(plaintext)
+    return output;
+
+
+print(decrypt("6e040b1d1d0b040b1d1d0b5f5c5d2e09030f0702400d0103"))
